2bd1b59feb1f263be4313788cd977c8008c318ff
Seth <sethvideo@gmail.com>
2013-04-11 20:26:41 -0700

    good config

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 
-$config['host'] = "localhost";
-$config['db'] = "castle_doctrine_1";
-$config['user'] = "root";
-$config['password'] = "root";
+$config['host'] = "db.doercreator.com";
+$config['db'] = "castle_fortify_1";
+$config['user'] = "castle_fortify";
+$config['password'] = "h7oKUxTp4nKc9jDPl3";
+
+error_reporting(E_ALL ^ E_STRICT & E_NOTICE);
+
