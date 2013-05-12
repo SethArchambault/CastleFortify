bd51e08a7fd818f7e610e51a6c67fa543b634dc1
Seth Archambault <seth@doercreator.com>
2013-05-12 16:40:50 -0400

    fortify js

---
+++ fortify.js
@@ -0,0 +1,538 @@
+
+// Set Variables
+
+var columns = 30;
+var rows = 30;
+var current_tile_name = "wooden-wall";
+var floor_tile_name = "empty-floor";
+var build_view = "normal";
+var save_disabled = false;
+var json_map_data;
+
+
+var doctrine_template = {
+    "empty-floor" : 0  , 
+    "wooden-wall" : 1  , 
+    "steel-wall" : 2  , 
+    "concrete-wall" : 3  , 
+    "window" : 20 , 
+    "doors" : 21 , 
+    "powered-door" : 30 , 
+    "wired-wooden-wall" : 51 , 
+    "pitbull" : 70 , 
+    "sticking-pressure-switch" : 100, 
+    "pressure-toggle-switch-off" : 101, 
+    "wire" : 102, 
+    "power" : 103, 
+    "voltage-triggered-switch" : 104, 
+    "voltage-triggered-inverted-switch" : 105, 
+    "wire-bridge" : 106, 
+    "rotary-toggle-switch" : 107, 
+    "pressure-toggle-switch-on" : 108, 
+    "electric-floor" : 110, 
+    "pit" : 111, 
+    "trap-door" : 112
+};
+
+  
+
+// Create Blocks
+
+for (var y = 0; y < rows; y++) 
+{
+    if (y == 14) 
+    {
+        $('#home').append('<div id="entrance">></div>');
+    }
+    for (var x = 0; x < columns; x++) 
+    {
+        $('#home').append('<div class="square '+floor_tile_name+'" id="x'+x+'y'+y+'" data-current-tile="empty-floor"></div>');
+    }
+    $('#home').append('<div class="clear"></div>');
+}
+
+generateMap();
+
+// Click on tile = change the current tile
+
+$('.tile').click(function() {
+    current_tile_name = $(this).attr('data-current-tile');
+});
+
+// Click on square = change the tile to current tile
+// If already current tile, change to floor
+
+$('.square').click(function() {
+    if ($(this).attr('data-current-tile') == current_tile_name)
+    {
+        $(this).attr('class', 'square '+floor_tile_name);
+        $(this).attr('data-current-tile', floor_tile_name);
+    }
+    else
+    {
+        $(this).attr('class', 'square '+current_tile_name)
+        $(this).attr('data-current-tile', current_tile_name);
+    }
+    if (build_view == "normal")
+    {
+        generateCode();    
+    }
+});
+
+// Calculate Price from Tile
+
+function getPrice(tile)
+{
+    switch(tile)
+    {
+        case "empty-floor":
+        return 0;
+        case "wooden-wall":
+        return 10;
+        case "steel-wall":
+        return 20;
+        case "concrete-wall":
+        return 50;
+        case "doors":
+        return 20;
+        case "window":
+        return 15;
+        case "pit":
+        return 100;
+        case "power":
+        return 200;
+        case "wire":
+        return 5;
+        case "wired-wooden-wall":
+        return 20;
+        case "pressure-toggle-switch-off":
+        return 50;
+        case "pressure-toggle-switch-on":
+        return 50;
+        case "sticking-pressure-switch":
+        return 50;
+        case "rotary-toggle-switch":
+        return 50;
+        case "wire-bridge":
+        return 10;
+        case "voltage-triggered-switch":
+        return 20;
+        case "voltage-triggered-inverted-switch":
+        return 20;
+        case "powered-door":
+        return 100;
+        case "electric-floor":
+        return 50;
+        case "trap-door":
+        return 200;
+        case "pitbull":
+        return 200;
+        case "chiwawa":
+        return 100;
+        case "cat":
+        return 20;
+        case "daughter":
+        return 0;
+        case "vault":
+        return 0;
+        case "wife":
+        return 0;
+        default:
+        return 0;
+
+    }
+}
+
+// Generate Code from Map
+
+function generateCode()
+{
+    switch($('#code_selection').val())
+    {
+        case 'doctrine':
+            generateDoctrine();
+            return;
+        case 'json':
+            generateJson();
+            return;
+        case 'custom':
+            generateCustom();
+            return;
+        default:
+            generateJson();
+            return;
+    }
+}
+
+
+function generateDoctrine()
+{
+    var doctrine = "";
+    var tile = ""; 
+    var total_price = 0;
+    for (var y = 0; y < rows; y++) 
+    {
+        for (var x = 0; x < columns; x++) 
+        {
+            tile = $('#x'+x+'y'+y).attr('data-current-tile');
+            doctrine += doctrine_template[tile] + "#";
+            if (doctrine_template[tile] === undefined)
+            {
+                console.log(tile);
+            }
+            total_price += getPrice(tile);
+        }
+    }
+    doctrine = doctrine.slice(0, -1);
+
+    if (total_price > 0)
+    {
+        $('#total_price_js').html("<strong>Value</strong> $"+total_price);
+    }
+    $('#data').val(doctrine);
+}
+
+
+
+
+function generateJson()
+{
+    var json = '{ "map" : [ \n';
+    var tile = ""; 
+    var total_price = 0;
+    for (var y = 0; y < rows; y++) 
+    {
+        for (var x = 0; x < columns; x++) 
+        {
+            tile = $('#x'+x+'y'+y).attr('data-current-tile');
+            if (tile != floor_tile_name) 
+            {
+                json += '  { "x" : '+x+', "y" : '+y+', "tile" : "'+tile+'" },\n';
+            }
+            total_price += getPrice(tile);
+        }
+    }
+    json = json.slice(0, -2);
+    json += '\n] }\n';
+
+    if (total_price > 0)
+    {
+        $('#total_price_js').html("<strong>Value</strong> $"+total_price);
+    }
+    json_map_data = json;
+    $('#data').val(json);
+}
+
+
+function generateCustom()
+{
+    var template_json = $('#data_template').val().trim();
+    try
+    {
+        template_data = $.parseJSON(template_json);
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+    var custom_language = '';
+    var tile = ""; 
+    var total_price = 0;
+    for (var y = 0; y < rows; y++) 
+    {
+        for (var x = 0; x < columns; x++) 
+        {
+            tile = $('#x'+x+'y'+y).attr('data-current-tile');
+            custom_language += template_data.template[tile];
+            total_price += getPrice(tile);
+        }
+        custom_language += "\n";
+    }
+
+    if (total_price > 0)
+    {
+        $('#total_price_js').html("<strong>Value</strong> $"+total_price);
+    }
+    // console.log('custom' + custom_language);
+
+    $('#data').val(custom_language);
+}
+
+function wireView(tile)
+{
+    switch(tile)
+    {
+        case "wooden-wall":
+        return "empty-floor";
+        case "steel-wall":
+        return "wire";
+        case "concrete-wall":
+        return "empty-floor";
+        case "doors":
+        return "empty-floor";
+        case "window":
+        return "empty-floor";
+        case "pit":
+        return "empty-floor";
+        case "wired-wooden-wall":
+        return "wire";
+        default:
+        return tile;
+    }
+}
+
+// this is the wrong way to do this.
+
+function generateMapFromJson()
+{
+    var json = $('#data').val().trim(); //json_map_data;
+    if (json == "")
+    {
+        return;
+    }
+    try {
+    var data = $.parseJSON(json);
+    } catch (e) {
+        return;
+    }
+
+    var total_price = 0;
+
+    for(var n = 0; n < data.map.length; n++)
+    {
+        var x = data.map[n].x;
+        var y = data.map[n].y;
+        var tile = data.map[n].tile;
+        // console.log('tile:'+tile);
+        if (build_view == "wire")
+        {
+            tile = wireView(tile);
+        }
+        $('#x'+x+'y'+y).attr('class', 'square '+tile);
+        $('#x'+x+'y'+y).attr('data-current-tile', tile);
+        total_price += getPrice(tile);
+    }
+    if (build_view == "normal" && total_price > 0)
+    {
+        $('#total_price_js').html("<strong>Value</strong> $"+total_price);    
+    }
+}
+
+
+function generateMapFromDoctrine()
+{
+    var doctrine_raw = $('#data').val().trim(); //json_map_data;
+    console.log(doctrine_raw);
+    if (doctrine_raw == "")
+    {
+        return;
+    }
+    data = doctrine_raw.split('#');
+
+
+    var total_price = 0;
+
+    var tile = "";
+    var x = 0;
+    var y = 0;
+    for(var n = 0; n < data.length; n++)
+    {
+        // find : 
+        // go down one line every x amount
+        if (data[n] == "\n") 
+        {
+            y++;
+            x=0;
+            continue;
+        }
+        tile = getKey(template_data.template, data[n]);
+        if (tile == "no-change")
+        {
+            x++;
+            continue;
+        }
+        // console.log(tile);
+        if (build_view == "wire")
+        {
+            tile = wireView(tile);
+        }
+        $('#x'+x+'y'+y).attr('class', 'square '+tile);
+        $('#x'+x+'y'+y).attr('data-current-tile', tile);
+        x++;
+        total_price += getPrice(tile);
+    }
+    if (build_view == "normal" && total_price > 0)
+    {
+        $('#total_price_js').html("<strong>Value</strong> $"+total_price);    
+    }
+}
+
+
+
+function getKey(collection, value)
+{   
+    var key = "empty-floor";
+    $.each(collection, function(k, v) {
+        if (v.trim() == value) 
+        {
+            key = k;
+        }
+    });
+    return key;
+
+}
+
+// generate map from custom
+// this should not really exist right.. it should just create the json
+
+function generateMapFromCustom()
+{
+    // console.log('custom generate');
+    var custom_map = $('#data').val().trim();
+    var template_json = $('#data_template').val().trim();
+    try
+    {
+        template_data = $.parseJSON(template_json);
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+
+    var total_price = 0;
+
+    var tile = "";
+    var x = 0;
+    var y = 0;
+    for(var n = 0; n < custom_map.length; n++)
+    {
+        if (custom_map[n] == "\n") 
+        {
+            y++;
+            x=0;
+            continue;
+        }
+        tile = getKey(template_data.template, custom_map[n]);
+        if (tile == "no-change")
+        {
+            x++;
+            continue;
+        }
+        // console.log(tile);
+        if (build_view == "wire")
+        {
+            tile = wireView(tile);
+        }
+        $('#x'+x+'y'+y).attr('class', 'square '+tile);
+        $('#x'+x+'y'+y).attr('data-current-tile', tile);
+        x++;
+        total_price += getPrice(tile);
+    }
+    if (build_view == "normal" && total_price > 0)
+    {
+        $('#total_price_js').html("<strong>Value</strong> $"+total_price);    
+    }
+}
+
+
+// generate map from code
+
+function generateMap()
+{
+    switch($('#code_selection').val())
+    {
+        case 'json':
+            generateMapFromJson();
+            return;
+        case 'custom':
+            generateMapFromCustom();
+            return;
+        case 'doctrine':
+            generateMapFromDoctrine();
+            return;
+        default:
+            generateMapFromJson();
+            return;
+    }
+
+}
+
+$('#load-code').click(function(e) {
+    e.preventDefault();
+    build_view = "normal";
+    $('#wire_view_js').prop('checked', false);
+    generateMap();
+});
+$('#save').click(function(e) {
+    $('#code_selection').val('json');
+    e.preventDefault();
+    build_view = "normal";
+    $('#wire_view_js').prop('checked', false);
+    generateMap();
+    generateCode();    
+
+    $('#save_and_share_js').modal();
+    $('#save-result').html("Saving...");
+    $.post('/save_map.php', 
+        {
+            "code" : $('#data').val().trim() 
+        }, 
+        function(data){
+            if (data.status == "success") 
+            {
+                $('#save-result').html("<div class='alert alert-info'>"+data.message + "<br><textarea class='span4'>" + data.link + "</textarea></div>");
+            }
+            else
+            {
+                $('#save-result').text("<div class='alert alert-error'>"+data.message+"</div>");
+                // $('#save-result').attr('class', 'alert alert-error');
+            }    
+        }, 'json');
+});
+$('#wire_view_js').click(function(e) {
+
+    if ($(this).prop('checked'))
+    {
+        build_view = "wire";
+        generateMap();
+        // $('#save').button('disabled');
+        // $('#save').removeClass('btn-primary');
+        // save_disabled = true;
+
+    }
+    else 
+    {
+        // $('#save').button('reset')
+        build_view = "normal";
+        generateMap();
+        generateCode();    
+        // $('#save').addClass('btn-primary');
+        // save_disabled = false;
+
+    }
+
+});
+
+$('#code_selection').change(function() {
+    switch ($(this).val())
+    {
+        case "json":
+            $('#data_template').hide();
+            generateCode();
+        break;
+        case "custom":
+            $('#data_template').show();
+            generateCode();
+        break;
+        default:
+            $('#data_template').hide();
+            generateCode();
+        break;
+    }
+});
+
+$('#data_template').change(function() {
+    build_view = "normal";
+    $('#wire_view_js').prop('checked', false);
+    generateCode();
+});
